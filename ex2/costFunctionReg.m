92f099a63f6a09e22abbf293fd808d04da11637d
Phani <phanikartcs@gmail.com>
2018-05-03 22:03:24 +0530

    ex3

--- ex2/costFunctionReg.m
+++ ex2/costFunctionReg.m
@@ -6,9 +6,10 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 
 % Initialize some useful values
 m = length(y); % number of training examples
-
+[n  waste] =  size(theta);
 % You need to return the following variables correctly 
 J = 0;
+t_sum = 0;
 grad = zeros(size(theta));
 
 % ====================== YOUR CODE HERE ======================
@@ -17,11 +18,43 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+alpha = 1;
+summ = 0;
+hx = 0;
+a=0;
+b=0;
+for i = 1:m
+	f = X(i,:);
+	hx = theta' * f';
+	hx = sigmoid(hx);
+	a = log(hx);
+	a = -1*a*y(i,1);
+	 
+	b = 1-hx;
+	b = log(b);
+	b=(1 - y(i,1))*b;
+	
+	a = a-b;
+	%hx = theta(1,1) + (theta(2,1) * X(i,2)); %h(tt)
+	%hx = hx - y(i,1);  % h(tt) - y
+	%hx = hx * hx;    %( h(tt) - y)^2
+    summ = summ + a;  %sigma( h(tt) - y)^2
+end
 
 
 
+J = summ / m;
 
 
+for i = 1:n
+	t_sum = t_sum + ( theta(i,1) * theta(i,1) );
+end
+t_sum = (t_sum * lambda)/(2*m);
+
+
+J = J + t_sum;
+grad = zeros(size(theta));
+display(J);
 % =============================================================
 
 end
